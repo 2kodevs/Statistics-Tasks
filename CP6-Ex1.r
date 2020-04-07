4ba095181842b3e5202081e10f3899bac4c12faa
Daniel Enrique <dcordb97@gmail.com>
2020-04-06 23:51:19 -0400

    Report done.

--- CP6-Ex1.r
+++ CP6-Ex1.r
@@ -9,8 +9,15 @@ den = sum(x * x) - sum(x) * sum(x) / n
 B1 = num / den
 B0 = (sum(y) - (B1 * sum(x))) / n
 
+png('disp_diagram.png')
+
 plot(x, y)
 abline(B0, B1)
+
+dev.off()
+
+B0
+B1
 
 print('For x = 1 predicted value is')
 print(B1 * 1 + B0)
